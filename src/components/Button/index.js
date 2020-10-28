3ac4f3c94a8f1b41687b8dde87cbfa5dc6c831ed
Ivan Nefagin <dfyz0r@list.ru>
2020-10-28 21:33:04 +0300

    refactor(Button): move Button style to StyledButton component

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,42 +1,6 @@
 import React from 'react';
-import styled from 'styled-components';
 
-const StyledButton = styled.button`
-  display: inline-block;
-  padding: 0 26px;
-
-  font: inherit;
-  font-size: 18px;
-  line-height: 40px;
-  font-weight: 700;
-  color: #fa9e39;
-  text-decoration: none;
-  text-transform: uppercase;
-  text-align: center;
-  vertical-align: middle;
-
-  background: transparent;
-  border: 3px solid #fa9e39;
-  border-radius: 25px;
-  outline: none;
-  cursor: pointer;
-  
-  transition: 0.15s ease-out;
-
-  &:hover,
-  &:focus {
-    color: #ffffff;
-    background-color: #fa9e39;
-  }
-
-  &:active {
-    opacity: 0.4;
-  }
-
-  @media (max-width: 767px) {
-    padding: 0 50px;
-  }
-`;
+import StyledButton from '../styled/StyledButton';
 
 const Button = ({ text, type }) => (
   <StyledButton type={type}>
