1242b09c28a01acd5b902d9c85a0894af9538a5b
Ivan Nefagin <dfyz0r@list.ru>
2020-10-28 21:47:47 +0300

    feat(Modal): add Modal component

---
+++ src/components/Catalog/Modal/index.js
@@ -0,0 +1,151 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import Button from '../../Button';
+
+const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  z-index: 998;
+  
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+  background-color: rgba(51, 51, 51, 0.5);
+  
+  animation-name: show;
+  animation-duration 0.3s;
+  
+  @keyframes show {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+  
+  @keyframes hide {
+    0% {
+      opacity: 1;
+    }
+    100% {
+      opacity: 0;
+    }
+  }
+`;
+
+const ModalBlock = styled.div`
+  display: flex;
+  width: 900px;
+  height: 600px;
+  
+  color: #333333;
+  background-color: #ffffff;
+`;
+
+const Image = styled.p`
+  flex-basis: 60%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0;
+  
+  img {
+    display: block;
+    width: 350px;
+    height: 350px;
+    
+    transition: 0.25s;
+    
+    &:hover {
+      transform: scale(1.05);
+    }
+    
+    &.pizza {
+      transform: rotate(15deg);
+      
+      &:hover {
+        transform: rotate(0) scale(1.05);
+      }
+    }
+  }
+`;
+
+const Content = styled.section`
+  flex-basis: 40%;
+  display: flex;
+  flex-direction: column;
+  padding: 40px 25px;
+  
+  background-color: #fcfcfc;
+  
+  h2 {
+    margin: 0;
+    margin-bottom: 20px;
+    
+    font-size: 28px;
+    line-height: 30px;
+    font-weight: 700;
+  }
+`;
+
+const Price = styled.p`
+  margin: 0;
+  margin-top: auto;
+  margin-bottom: 20px;
+  
+  font-size: 20px;
+  line-height: 28px;
+  font-weight: 500;
+  text-align: center;
+    
+  span {
+    font-size: 28px;
+    font-weight: 700;
+    
+    cursor: pointer;
+    transition: 0.3s;
+    
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+`;
+
+const Modal = ({ selectedItem, setSelectedItem }) => {
+
+  if (!selectedItem) return null;
+
+  const closeModal = evt => {
+    if (evt.target.id !== 'overlay') return;
+
+    evt.target.style.animationName = 'hide';
+    setTimeout(() => setSelectedItem(null), 250);
+  };
+
+  const { name, img, price, category } = selectedItem;
+
+  return (
+    <Overlay id="overlay" onClick={closeModal}>
+      <ModalBlock>
+        <Image>
+          <img className={category === 'pizza' ? category : ''} src={img} alt={name}/>
+        </Image>
+        <Content>
+          <h2>{name}</h2>
+          <Price>
+            Стоимость: <span>{price.toLocaleString('ru-RU', {style: 'currency', currency: 'RUB'})}</span>
+          </Price>
+          <Button text="Добавить в корзину" type="button" />
+        </Content>
+      </ModalBlock>
+    </Overlay>
+  );
+};
+
+export default Modal;
