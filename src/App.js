223e13163fd77712897e5343aded4d7ce8561f44
Ivan Nefagin <dfyz0r@list.ru>
2020-10-30 01:52:10 +0300

    feat(App): import Cart component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './css/fonts.css';
 import GlobalStyle from './components/styled/GlobalStyle';
 import Header from './components/Header';
 import Catalog from './components/Catalog';
+import Cart from './components/Cart';
 import Modal from './components/Catalog/Modal';
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
     <>
       <GlobalStyle />
       <Header />
+      <Cart />
       <Catalog setSelectedItem={setSelectedItem} />
       <Modal selectedItem={selectedItem} setSelectedItem={setSelectedItem} />
     </>
