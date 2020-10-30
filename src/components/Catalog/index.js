0fd9865f0e8d7f5936b1415a6e4c081f0aeea414
Ivan Nefagin <dfyz0r@list.ru>
2020-10-30 13:36:17 +0300

    move Banner component

--- src/components/Catalog/index.js
+++ src/components/Catalog/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import Banner from '../Banner';
+import Banner from './Banner';
 import CatalogList from './CatalogList';
 
 import database from './database';
