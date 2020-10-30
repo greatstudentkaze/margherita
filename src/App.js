8a0b249f879241e364f37f4453dd8edc87364a12
Ivan Nefagin <dfyz0r@list.ru>
2020-10-30 12:25:31 +0300

    feat(App): import hooks and pass states to Cart and Modal

--- src/App.js
+++ src/App.js
@@ -8,18 +8,20 @@ import Catalog from './components/Catalog';
 import Cart from './components/Cart';
 import Modal from './components/Catalog/Modal';
 
-import useSelectedItem from './components/useSelectedItem';
+import useSelectedItem from './components/hooks/useSelectedItem';
+import useCart from './components/hooks/useCart';
 
 const App = () => {
   const selectedItem = useSelectedItem();
+  const cart = useCart();
 
   return (
     <>
       <GlobalStyle />
       <Header />
-      <Cart />
+      <Cart {...cart} />
       <Catalog {...selectedItem} />
-      {selectedItem.selectedItem && <Modal {...selectedItem} />}
+      {selectedItem.selectedItem && <Modal {...selectedItem} {...cart} />}
     </>
   );
 }
