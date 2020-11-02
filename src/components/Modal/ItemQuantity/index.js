7644a5898de9e8ad8856d25e0874baa17948f88d
Ivan Nefagin <dfyz0r@list.ru>
2020-11-02 13:25:32 +0300

    feat(ItemQuantity): add base styles

--- src/components/Modal/ItemQuantity/index.js
+++ src/components/Modal/ItemQuantity/index.js
@@ -4,11 +4,33 @@ import styled from 'styled-components';
 const Quantity = styled.div`
   display: flex;
   justify-content: space-between;
+  align-item: center;
+  
+  font-size: 20px;
+  line-height: 40px;
+  color: #333333;
 `;
 
 const QuantityInput = styled.input`
-  width: 50px;
-  font-size: 20px;
+  padding: 0 10px;
+  
+  font: inherit;
+  line-height: 38px;
+  font-weight: 700;
+  text-align: center;
+  vertical-align: top;
+  
+  background-color: #ffffff;
+  border: none;
+  border-top: 1px solid #e7e7e7;
+  border-bottom: 1px solid #e7e7e7;
+  outline: none;
+  
+  transition: 0.2s ease-out;
+  
+  &:focus {
+    border-color: #9a9a9a;
+  }
   
   -moz-appearance: textfield;
   
@@ -20,22 +42,50 @@ const QuantityInput = styled.input`
 `;
 
 const QuantityButton = styled.button`
+  min-width: 28px;
+  padding: 0 10px;
+
   font: inherit;
+  line-height: 38px;
+  font-weight: 700;
+  text-align: middle;
+  vertical-align: top;
   
-  background-color: transparent;
+  background-color: #ffffff;
+  border: 1px solid #e7e7e7;
+  outline: none;
   
   cursor: pointer;
+  transition: 0.2s ease-out;
+  
+  &:hover,
+  &:focus {
+    border-color: #9a9a9a;
+  }
+  
+  &:disabled {
+    background-color: #efefef;
+    border-color: #e7e7e7;
+    cursor: not-allowed;
+  }
 `;
 
 const ItemQuantity = ({ quantity, setQuantity, handleChange }) => {
 
   return (
     <Quantity>
-      <span>Количество</span>
+      Количество
       <div>
-        <QuantityButton onClick={() => setQuantity(prevQuantity => prevQuantity - 1)} disabled={quantity <= 1}>-</QuantityButton>
-        <QuantityInput type="number" min="1" max="20" step="1" value={quantity < 1 ? 1 : quantity} onChange={handleChange} />
-        <QuantityButton onClick={() => setQuantity(prevQuantity => prevQuantity + 1)}>+</QuantityButton>
+        <QuantityButton
+          onClick={() => setQuantity(prevQuantity => prevQuantity - 1)}
+          disabled={quantity <= 1}>-</QuantityButton>
+        <QuantityInput
+          type="number" min="1" max="20" step="1" readOnly
+          value={quantity < 1 ? 1 : quantity} o
+          nChange={handleChange} />
+        <QuantityButton
+          onClick={() => setQuantity(prevQuantity => prevQuantity + 1)}
+          disabled={quantity > 19}>+</QuantityButton>
       </div>
     </Quantity>
   );
