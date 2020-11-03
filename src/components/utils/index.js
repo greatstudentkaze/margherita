4f5e1b899354d2b288efd7b325a40a3e43b8d2c2
Ivan Nefagin <dfyz0r@list.ru>
2020-11-03 13:51:27 +0300

    feat(getTotalPrice): change calculation of totalPrice

--- src/components/utils/index.js
+++ src/components/utils/index.js
@@ -1,3 +1,9 @@
-export const getTotalPrice = order => order.price * order.quantity;
+export const getTotalPrice = order => {
+  const toppingsCount = order.toppings && order.toppings.filter(topping => topping.selected).length;
+  const toppingPrice = 0.1;
+  const toppingsPrices = (order.price * toppingPrice) * toppingsCount;
+
+  return (order.price + toppingsPrices) * order.quantity;
+}
 
 export const formatPrice = price => price.toLocaleString('ru-RU', {style: 'currency', currency: 'RUB'});
