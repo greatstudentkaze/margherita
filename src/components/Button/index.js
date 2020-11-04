fe12aba2f065bb9de7ab6b32e1fc8258f1e1968f
Ivan Nefagin <dfyz0r@list.ru>
2020-11-04 12:48:59 +0300

    feat(Button): pass all remaining props

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 import StyledButton from '../Styled/StyledButton';
 
-const Button = ({ text, type, handleClick }) => (
-  <StyledButton type={type} onClick={handleClick}>
+const Button = ({ text, type, handleClick, ...props }) => (
+  <StyledButton type={type} onClick={handleClick} {...props}>
     {text}
   </StyledButton>
 );
