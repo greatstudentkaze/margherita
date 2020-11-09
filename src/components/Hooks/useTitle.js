42e0cf9890eb03b7afdc428539dc06656bdfec3f
Ivan Nefagin <dfyz0r@list.ru>
2020-11-09 11:50:48 +0300

    feat(Hooks): add a useEffect hook to change the document.title depending on the selectedItem

---
+++ src/components/Hooks/useTitle.js
@@ -0,0 +1,14 @@
+import { useEffect } from 'react';
+
+const useTitle = ({ selectedItem }) => {
+  const title = selectedItem
+    ? selectedItem.category === 'pizza'
+      ? `Пицца - ${selectedItem.name}` : selectedItem.name
+    : 'Margherita: осторожно вкусная пицца!';
+
+  useEffect(() => {
+    document.title = title;
+  });
+};
+
+export default useTitle;
