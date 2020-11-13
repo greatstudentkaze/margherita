a03ce7246abdc83257fa487ca88c109d979e06b1
Ivan Nefagin <dfyz0r@list.ru>
2020-11-13 10:03:51 +0300

    feat(Modal): move styled components to Modal\Styled

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,5 +1,4 @@
 import React, { useContext } from 'react';
-import styled, { css } from 'styled-components';
 
 import Context from '../utils/context';
 
@@ -13,85 +12,7 @@ import useToppings from '../Hooks/useToppings';
 import useChoice from '../Hooks/useChoice';
 import { getTotalPrice, formatPrice } from '../utils';
 
-import { Overlay } from './Styled';
-
-const ModalBlock = styled.div`
-  display: flex;
-  width: 900px;
-  height: 600px;
-  
-  color: #333333;
-  background-color: #ffffff;
-`;
-
-const Image = styled.p`
-  flex-basis: 60%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin: 0;
-  
-  img {
-    display: block;
-    width: 350px;
-    height: 350px;
-    
-    transition: 0.25s;
-    
-    &:hover {
-      transform: scale(1.05);
-    }
-    
-    ${props => props.category === 'pizza' && css`
-      transform: rotate(15deg);
-      
-      &:hover {
-        transform: rotate(0) scale(1.05);
-      }
-    `}
-  }
-`;
-
-const Content = styled.section`
-  flex-basis: 40%;
-  display: flex;
-  flex-direction: column;
-  padding: 40px 25px;
-  
-  background-color: #fcfcfc;
-  
-  h2 {
-    margin: 0;
-    margin-bottom: 20px;
-    
-    font-size: 28px;
-    line-height: 30px;
-    font-weight: 700;
-  }
-`;
-
-const Price = styled.p`
-  margin: 0;
-  margin-top: auto;
-  margin-bottom: 20px;
-  
-  font-size: 20px;
-  line-height: 28px;
-  font-weight: 500;
-  text-align: center;
-    
-  span {
-    font-size: 28px;
-    font-weight: 700;
-    
-    cursor: pointer;
-    transition: 0.3s;
-    
-    &:hover {
-      text-decoration: underline;
-    }
-  }
-`;
+import { Overlay, ModalBlock, Image, Content, Price } from './Styled';
 
 const Modal = () => {
   const { selectedItem: { selectedItem, setSelectedItem }, cart: { orders, setOrders } } = useContext(Context);
