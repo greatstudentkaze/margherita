7c9276a99b1781c2e0c7543adc62aebdbb684d3d
Ivan Nefagin <dfyz0r@list.ru>
2020-11-19 22:28:25 +0300

    feat(functions): add functional of sending an order by email

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,38 @@
 const functions = require('firebase-functions');
+const nodemailer = require('nodemailer'),
+  htmlToText = require('nodemailer-html-to-text').htmlToText;
+const { email, password } = require('./config');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: email,
+    pass: password
+  }
+});
+
+transporter.use('compile', htmlToText());
+
+const sendOrderByEmail = data => {
+  const options = {
+    from: 'Margherita',
+    to: data.email,
+    subject: 'Заказ получен (Margherita)',
+    html: `
+      <section>
+        <h2>Спасибо, ${data.clientName}! Мы работаем над Вашим заказом.</h2>
+        <ul>
+          ${data.order.map(({ name, price, quantity }) => (
+            `<li>${name} - ${quantity} шт., цена ${price * quantity} руб.</li>`
+          )).join('')}    
+        </ul>
+        <p>Итого: ${data.order.reduce((sum, item) => sum + item.price * item.quantity, 0)} руб.</p>
+      </section>
+    `
+  };
+
+  transporter.sendMail(options);
+};
+
+exports.sendEmailToUser = functions.database.ref('orders/{pushID}')
+  .onCreate(order => sendOrderByEmail(order.val()));
