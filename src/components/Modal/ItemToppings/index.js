92a6d5f6116b093942da8e53387841a7a084a2e2
Ivan Nefagin <dfyz0r@list.ru>
2020-11-28 01:02:12 +0300

    feat(ItemToppings): add styles

--- src/components/Modal/ItemToppings/index.js
+++ src/components/Modal/ItemToppings/index.js
@@ -3,8 +3,17 @@ import styled from 'styled-components';
 
 import ItemContext from '../ItemContext';
 
-const StyledToppings = styled.div`
+import check from '../../../img/check.svg';
 
+const StyledToppings = styled.div`
+  font-size: 20px;
+  line-height: 24px;
+  color: #333333;
+`;
+
+const ToppingsHeading = styled.p`
+  margin: 0;
+  margin-bottom: 12px;
 `;
 
 const ToppingsWrapper = styled.div`
@@ -14,13 +23,80 @@ const ToppingsWrapper = styled.div`
 `;
 
 const ToppingLabel = styled.label`
+  position: relative;
+
   display: block;
+  margin-left: 40px;
+  padding-top: 3px;
+  padding-bottom: 3px;
+  
+  font-size: 24px;
+  line-height: 24px;
+  
   cursor: pointer;
+  user-select: none;
+  transition: 0.2s;
+  
+  &:hover {
+    color: #333333bd;
+  }
 `;
 
-const ToppingCheckbox = styled.input`
-  margin-right: 5px;
+const ToppingInput = styled.input`
+  &:checked + span {
+    background-color: #fa9e39;
+  }
+  
+  &:checked + span::after {
+    visibility: visible;
+  }
+  
+  &:focus-visible + span {
+    box-shadow 0 0 0 2px #ffffff, 0 0 0 5px #cd3a13;
+  }
+`;
+
+const ToppingCheckbox = styled.span`
+  position: absolute;
+  top: 0;
+  left: -40px;
+  
+  display: inline-block;
+  width: 30px;
+  height: 30px;
+  
+  background-color: #fcfcfc;
+  border: 2px solid #fa9e39;
+  border-radius: 8px;
+  
   cursor: pointer;
+  transition: 0.2s;
+  
+  &::after {
+    content: "";
+    
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    
+    visibility: hidden;
+    width: 20px;
+    height: 20px;
+    
+    background-image: url(${check});
+    background-repeat: no-repeat;
+    background-size: 20px;
+    background-position: center;
+  }
+  
+  &:hover {
+    border-color: #ff8500;
+    
+    input:checked + & {
+      background-color: #ff8500;
+    }
+  }
 `;
 
 const ItemToppings = () => {
@@ -28,15 +104,17 @@ const ItemToppings = () => {
 
   return (
     <StyledToppings>
-      <p>Добавить в пиццу</p>
+      <ToppingsHeading>Добавить в пиццу:</ToppingsHeading>
       <ToppingsWrapper>
         {toppings.map((topping, i) => (
           <ToppingLabel key={i}>
-            <ToppingCheckbox
+            <ToppingInput
+              className="visually-hidden"
               type="checkbox"
               checked={topping.selected}
               onChange={() => toggleTopping(i)}
             />
+            <ToppingCheckbox />
             {topping.name}
           </ToppingLabel>
         ))}
