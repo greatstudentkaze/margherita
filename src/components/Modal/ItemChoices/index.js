bec756dc76df1f0cec85895d1d3286507a402b2c
Ivan Nefagin <dfyz0r@list.ru>
2021-01-06 00:35:05 +0300

    feat(ItemChoices): add styles

--- src/components/Modal/ItemChoices/index.js
+++ src/components/Modal/ItemChoices/index.js
@@ -4,23 +4,96 @@ import styled from "styled-components";
 import ItemContext from '../ItemContext';
 
 const StyledChoices = styled.div`
+  font-size: 20px;
+  line-height: 24px;
+  color: #333333;
+`;
 
+const ChoicesHeading = styled.p`
+  margin: 0;
+  margin-bottom: 12px;
 `;
 
 const ChoicesWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  row-gap: 10px;
+  row-gap: 5px;
 `;
 
 const ChoiceLabel = styled.label`
+  position: relative;
+
   display: block;
+  margin-bottom: 5px;
+  margin-left: 40px;
+  padding-top: 3px;
+  padding-bottom: 3px;
+  
+  font-size: 24px;
+  line-height: 24px;
+  
   cursor: pointer;
+  user-select: none;
+  transition: 0.2s;
+  
+  &:hover {
+    color: #333333bd;
+  }
+  
+  &:last-child {
+    margin-bottom: 0;
+  }
 `;
 
-const ChoiceRadio = styled.input`
-  margin-right: 5px;
+const ChoiceInput = styled.input`
+  &:checked + span::after {
+    visibility: visible;
+  }
+  
+  &:focus-visible + span {
+    box-shadow: 0 0 0 2px #ffffff, 0 0 0 5px #cd3a13;
+  }
+`;
+
+const ChoiceRadio = styled.span`
+  position: absolute;
+  top: 0;
+  left: -40px;
+  
+  display: inline-block;
+  width: 30px;
+  height: 30px;
+  
+  background-color: #fcfcfc;
+  border: 3px solid #fa9e39;
+  border-radius: 50%;
+  
   cursor: pointer;
+  transition: 0.2s;
+  
+  &::after {
+    content: "";
+    
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    
+    visibility: hidden;
+    width: 15px;
+    height: 15px;
+
+    background-color: #fa9e39;
+    border-radius: 50%;
+  }
+  
+  &:hover {
+    border-color: #ff8500;
+    
+    input:checked + & {
+      background-color: #ff8500;
+    }
+  }
 `;
 
 const ItemChoices = () => {
@@ -28,17 +101,19 @@ const ItemChoices = () => {
 
   return (
     <StyledChoices>
-      <p>Выберите:</p>
+      <ChoicesHeading>Выберите:</ChoicesHeading>
       <ChoicesWrapper>
         {selectedItem.choices.map((itemChoice, i) => (
           <ChoiceLabel key={i}>
-            <ChoiceRadio
+            <ChoiceInput
+              className="visually-hidden"
               type="radio"
               name="choices"
               checked={itemChoice === choice}
               value={itemChoice}
               onChange={changeChoice}
             />
+            <ChoiceRadio />
             {itemChoice}
           </ChoiceLabel>
         ))}
